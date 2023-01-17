c37799e31b43dbcd13c2d404fd69f8d1ce26a3a2
chiclaim <chiclaim@gmail.com>
2023-01-17 19:15:53 +0800

    应用专属目录 API

--- app/src/main/java/com/chiclaim/android/storage/AppSpecificDirAPI.kt
+++ app/src/main/java/com/chiclaim/android/storage/AppSpecificDirAPI.kt
@@ -1,5 +1,6 @@
 package com.chiclaim.android.storage
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Environment
 
@@ -12,23 +13,42 @@ class AppSpecificDirAPI {
 
     companion object {
 
-        fun listDirs(context: Context) {
-            // -------- 应用专属目录（沙盒目录），自己访问无需权限，其他应用无法访问 --------
-            // /data/user/0/com.chiclaim.android.storage/files
-            println(context.filesDir)
-            // /data/user/0/com.chiclaim.android.storage/cache
-            println(context.cacheDir)
-            // /data/user/0/com.chiclaim.android.storage/app_images // 会加上 app_ 前缀
-            println(context.getDir("images", Context.MODE_PRIVATE))
+        @SuppressLint("SetTextI18n")
+        fun buildDirsStr(context: Context):String {
+            return buildString {
+                // -------- 应用专属目录（沙盒目录），自己访问无需权限，其他应用无法访问 --------
+                // /data/user/0/com.chiclaim.android.storage/files
+                // /data/user/999/com.chiclaim.android.storage/files   应用双开
+                append("context.filesDir:\n")
+                append(context.filesDir.absolutePath)
+                append("\n\n")
 
-            // -------- 应用专属目录（外部存储），Android4.4及以上不需要权限，其他应用无法访问 --------
-            // 缓存目录：/storage/emulated/0/Android/data/com.chiclaim.android.storage/cache
-            println(context.externalCacheDir)
-            // files 的根目录： /storage/emulated/0/Android/data/com.chiclaim.android.storage/files
-            println(context.getExternalFilesDir(null))
-            // files 的子目录(Music)： /storage/emulated/0/Android/data/com.chiclaim.android.storage/files/Music
-            println(context.getExternalFilesDir(Environment.DIRECTORY_MUSIC))
+                // /data/user/0/com.chiclaim.android.storage/cache
+                append("context.cacheDir:\n")
+                append(context.cacheDir.absolutePath)
+                append("\n\n")
 
+                // /data/user/0/com.chiclaim.android.storage/app_images // 会加上 app_ 前缀
+                append("context.getDir(\"image\"),Context.MODE_PRIVATE:\n")
+                append(context.getDir("images", Context.MODE_PRIVATE).absolutePath)
+                append("\n\n")
+
+                // -------- 应用专属目录（外部存储），Android4.4及以上不需要权限，其他应用无法访问 --------
+                // 缓存目录：/storage/emulated/0/Android/data/com.chiclaim.android.storage/cache
+                append("context.externalCacheDir:\n")
+                append(context.externalCacheDir?.absolutePath)
+                append("\n\n")
+
+                // files 的根目录： /storage/emulated/0/Android/data/com.chiclaim.android.storage/files
+                append("context.getExternalFilesDir(null):\n")
+                append(context.getExternalFilesDir(null)?.absolutePath)
+                append("\n\n")
+
+                // files 的子目录(Music)： /storage/emulated/0/Android/data/com.chiclaim.android.storage/files/Music
+                append("context.getExternalFilesDir(Environment.DIRECTORY_MUSIC):\n")
+                append(context.getExternalFilesDir(Environment.DIRECTORY_MUSIC)?.absolutePath)
+                append("\n\n")
+            }
         }
     }
 
--- app/src/main/java/com/chiclaim/android/storage/MainActivity.kt
+++ app/src/main/java/com/chiclaim/android/storage/MainActivity.kt
@@ -2,12 +2,14 @@ package com.chiclaim.android.storage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        AppSpecificDirAPI.listDirs(this)
+        val textView = findViewById<TextView>(R.id.text)
+        textView.text = AppSpecificDirAPI.buildDirsStr(this)
     }
 
 
